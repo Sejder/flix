08e1bb587a1c9035788f7b24f532b274573bebe9
Sejder <mikkel.sejdelin@gmail.com>
2026-04-11 16:35:49 +0200

    removed tests

--- main/test/ca/uwaterloo/flix/language/phase/TestParserHappy.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestParserHappy.scala
@@ -44,22 +44,4 @@ class TestParserHappy extends AnyFunSuite with TestUtils {
     val result = check(input, Options.TestWithLibMin)
     expectSuccess(result)
   }
-
-  test("DeepStatementChain.01") {
-    // Regression test: 1000 statement expressions must not cause stack overflow.
-    val n = 1000
-    val stmts = (1 to n).map(_ => "f()").mkString("; ")
-    val input = s"def f(): Unit \\ IO = checked_ecast(())\ndef foo(): Int32 \\ IO = { $stmts; 123 }"
-    val result = check(input, Options.TestWithLibMin)
-    expectSuccess(result)
-  }
-
-  test("DeepStatementChain.02") {
-    // Regression test: deep statement chain inside a let-binding.
-    val n = 1000
-    val stmts = (1 to n).map(_ => "f()").mkString("; ")
-    val input = s"def f(): Unit \\ IO = checked_ecast(())\ndef foo(): Int32 \\ IO = { let _x = 42; $stmts; 123 }"
-    val result = check(input, Options.TestWithLibMin)
-    expectSuccess(result)
-  }
 }
